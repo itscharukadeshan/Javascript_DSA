e59631c079f0ea5fefd020bc42c9e3c5672c75ef
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-19 08:03:36 +0000

    add sum example

---
+++ examples/getSum.js
@@ -0,0 +1,14 @@
+/** @format */
+
+const getSum = (A, B) => {
+  const firstNumber = A;
+  const secondNumber = B;
+
+  const sumOfNumbers = firstNumber + secondNumber;
+
+  return sumOfNumbers;
+};
+
+const result = getSum(40, 2);
+
+console.log(result);
