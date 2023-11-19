a02e27f53fe6c5630b2f911861e72937f77ef6a2
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-19 08:36:30 +0000

    add case count letters function

---
+++ 01_countLetters/countLetters.js
@@ -0,0 +1,16 @@
+/** @format */
+
+const CountLetters = (str, letter) => {
+  let count = 0;
+  let string = str.toLowerCase();
+  let letterInLowercase = letter.toLowerCase();
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === letterInLowercase) {
+      count++;
+    }
+  }
+  return `Letter ${letter} is present ${count} times in ${str}`;
+};
+
+console.log(CountLetters("My Name", "M"));
