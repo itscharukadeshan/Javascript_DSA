deffc0937a9a1d6c54ed9e8aeb47a24438d17ad7
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-20 08:14:16 +0000

    add find max with for loops and built js

---
+++ 02_findMax/findMax.js
@@ -0,0 +1,36 @@
+/** @format */
+// * Built in method
+
+/*
+
+const findMax = (array) => {
+  const result = Math.max(...array);
+
+  return result;
+};
+
+console.log(findMax([1, 5, 3, 2, 7, 10, 5, 15]));
+
+*/
+
+// * With For Loops
+
+/*
+
+const findMaxWithForLoop = (array) => {
+  let max = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+
+  const result = max;
+
+  return result;
+};
+
+console.log(findMaxWithForLoop([1, 2, 5, 8, 9, 10, 12]));
+
+*/
