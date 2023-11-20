3f7aba7e8428b497e674ecf4387274723c41540c
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-20 10:02:07 +0000

    add code for Without built in methods and regex

--- 05_checkPalindrome/checkPalindrome.js
+++ 05_checkPalindrome/checkPalindrome.js
@@ -18,3 +18,62 @@ const result_3 = checkPalindrome("Not and palindrome");
 console.log(`Is "Race Car" a palindrome? => ${result_1}`);
 console.log(`Is "racecar" a palindrome? => ${result_2}`);
 console.log(`Is "Not a palindrome" a palindrome? => ${result_3}`);
+
+// * Without built in methods and regex
+
+const isPalindrome = (string) => {
+  const formattedString = isLetterOrNumber(string.toLowerCase());
+
+  const reverseStr = reverseString(formattedString);
+
+  const result = formattedString === reverseStr;
+
+  return result;
+};
+
+const isLetterOrNumber = (string) => {
+  let formattedString = "";
+
+  for (let i = 0; i < string.length; i++) {
+    let character = string[i];
+
+    if (removeSpecialCharacters(character)) {
+      formattedString += character;
+    }
+  }
+
+  const result = formattedString;
+
+  return result;
+};
+
+const removeSpecialCharacters = (character) => {
+  // ! use the unicode code range instead of regex
+
+  const code = character.charCodeAt(0);
+
+  return (
+    (code >= 48 && code <= 57) /* * Numbers unicode range */ ||
+    (code >= 97 && code <= 122) /* * Lowercase letter unicode range */
+  );
+};
+
+const reverseString = (string) => {
+  let reverse = "";
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverse += string[i];
+  }
+
+  const result = reverse;
+
+  return result;
+};
+
+const result_4 = isPalindrome("Race Car");
+const result_5 = isPalindrome("racecar");
+const result_6 = isPalindrome("Not and palindrome");
+
+console.log(`Is "Race Car" a palindrome? => ${result_4}`);
+console.log(`Is "racecar" a palindrome? => ${result_5}`);
+console.log(`Is "Not a palindrome" a palindrome? => ${result_6}`);
