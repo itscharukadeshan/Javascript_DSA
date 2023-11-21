fe229e070344c92966cedfe1d74b5c27aaa5cc03
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-21 07:29:29 +0000

    Using Set and for of loops

--- 01_General_Challanges/commonNumbers.js
+++ 01_General_Challanges/commonNumbers.js
@@ -1,4 +1,9 @@
 /** @format */
+
+// * Using for loops
+
+/*
+
 const commonNumbers = (array_01, array_02) => {
   const commonNumArray = [];
 
@@ -14,5 +19,22 @@ const commonNumbers = (array_01, array_02) => {
 
   return result;
 };
+*/
 
+// * Using Set and for of loops
+
+const commonNumbers = (array_01, array_02) => {
+  const set_01 = new Set(array_01);
+  const commonNumArray = [];
+
+  for (let num of array_02) {
+    if (set_01.has(num)) {
+      commonNumArray.push(num);
+    }
+  }
+
+  const result = commonNumArray;
+
+  return result;
+};
 console.log(commonNumbers([1, 3, 5, 7, 2], [1, 3, 5, 9, 0]));
