546acc59c590b10c7d39f17d54f2a077c009c901
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-21 09:40:41 +0000

    add one more comment

--- 01_General_Challanges/03_findMissingNumber/findMissingNumber.js
+++ 01_General_Challanges/03_findMissingNumber/findMissingNumber.js
@@ -31,6 +31,10 @@ console.log(
 
 */
 
+/* console.log(`Missing number = ${missing}`); */
+
+// * Using for loops to find current number
+
 const findMissingNumber = (array) => {
   if (array.length === 0) return 1;
 
@@ -68,4 +72,40 @@ const findMissingNumber = (array) => {
   return result;
 };
 
-console.log(findMissingNumber(NumberArray));
+console.log(`Using for loops | ${findMissingNumber(NumberArray)}`);
+
+const findMissingNumberWithReduce = (array) => {
+  if (array.length === 0) return 1;
+
+  const n = array.length + 1;
+
+  const expectedSum = (n * (n + 1)) / 2;
+
+  /*
+
+  console.log(`Expected sum = ${expectedSum}`);
+
+  */
+
+  const currentSum = array.reduce((sum, num) => sum + num, 0);
+
+  /*
+
+  console.log(`Current Sum = ${currentSum}`);
+
+  */
+
+  const result = expectedSum - currentSum;
+
+  /*
+
+  console.log(
+    `Result = expectedSum(${expectedSum}) - currentSum(${currentSum})`
+  );
+
+  */
+
+  return result;
+};
+
+console.log(`Using reduce | ${findMissingNumberWithReduce(NumberArray)}`);
