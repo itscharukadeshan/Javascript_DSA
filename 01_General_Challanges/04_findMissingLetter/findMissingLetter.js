dc5c8e034dede9bef46049842431d360839e852d
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-22 07:43:34 +0000

    add find missing letter code

--- 01_General_Challanges/04_findMissingLetter/findMissingLetter.js
+++ 01_General_Challanges/04_findMissingLetter/findMissingLetter.js
@@ -1,5 +1,23 @@
 /** @format */
 
+// * Using for loops
+
 const findMissingLetter = (letterArray) => {
-  const alphabet = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  const startIndex = alphabet.indexOf(letterArray[0]);
+
+  for (let i = 0; i < letterArray.length; i++) {
+    if (letterArray[i] !== alphabet[startIndex + i]) {
+      return alphabet[startIndex + i];
+    }
+  }
+
+  return "";
+};
+
+console.log(findMissingLetter(["a", "b", "c", "d", "f"]));
+
+const findMissingLetterWithUniCode = (letterArray) => {
+  const start = letterArray[0].charCodeAt(0);
 };
