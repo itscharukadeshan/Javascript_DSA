93a7a1b873abc3bc561a523b045fd6a5dbebb95a
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-22 08:37:41 +0000

    add find Unique Characters With Set

--- 01_General_Challanges/05_FindUniqueChartres/FindUniqueChartres.js
+++ 01_General_Challanges/05_FindUniqueChartres/FindUniqueChartres.js
@@ -21,3 +21,26 @@ console.log(findUniqueCharacters("StringWithRepeatingLetters"));
 console.log(findUniqueCharacters("abcd ABCD"));
 
 console.log(findUniqueCharacters("singular"));
+
+
+const findUniqueCharactersWithSet = (letterString) => {
+  const characters = new Set()
+
+  for (let i = 0; i < letterString.length; i++) {
+    const character = letterString[i];
+
+    if (characters.has(character)) {
+      return false;
+    }
+
+    characters.add(character)
+  }
+
+  return true;
+};
+
+console.log(findUniqueCharactersWithSet("StringWithRepeatingLetters"));
+
+console.log(findUniqueCharactersWithSet("abcd ABCD"));
+
+console.log(findUniqueCharactersWithSet("singular"));
