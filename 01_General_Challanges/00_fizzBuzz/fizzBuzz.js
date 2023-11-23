0c790d37f27615b2fa323d859956084b337db5dd
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-23 17:46:20 +0000

    fix the missing check

--- 01_General_Challanges/00_fizzBuzz/fizzBuzz.js
+++ 01_General_Challanges/00_fizzBuzz/fizzBuzz.js
@@ -28,7 +28,7 @@ const fizzBuzzWithFistCommonNum = (number) => {
   const array = [];
 
   for (let i = 1; i <= number; i++) {
-    if (i % 15) {
+    if (i % 15 === 0) {
       array.push("FizzBuzz");
     } else if (i % 3 === 0) {
       array.push("Fizz");
