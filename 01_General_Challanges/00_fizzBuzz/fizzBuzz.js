c101a9aa647ed2f3739fa0cff37cf77e48812fa1
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-23 17:44:32 +0000

    add  fizzBuzzWithFistCommonNum

--- 01_General_Challanges/00_fizzBuzz/fizzBuzz.js
+++ 01_General_Challanges/00_fizzBuzz/fizzBuzz.js
@@ -23,3 +23,25 @@ const fizzBuzz = (number) => {
 };
 
 console.log(fizzBuzz(15));
+
+const fizzBuzzWithFistCommonNum = (number) => {
+  const array = [];
+
+  for (let i = 1; i <= number; i++) {
+    if (i % 15) {
+      array.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      array.push("Fizz");
+    } else if (i % 5 === 0) {
+      array.push("Buzz");
+    } else {
+      array.push(i);
+    }
+  }
+
+  const result = array;
+
+  return result;
+};
+
+fizzBuzzWithFistCommonNum(30);
