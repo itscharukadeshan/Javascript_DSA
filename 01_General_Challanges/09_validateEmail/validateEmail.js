058ece8e0c63cef32326ca702c30ab41a4e8892c
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-24 08:35:42 +0000

    add first email validation

--- 01_General_Challanges/09_validateEmail/validateEmail.js
+++ 01_General_Challanges/09_validateEmail/validateEmail.js
@@ -1,3 +1,13 @@
 /** @format */
 
-const validateEmail = () => {};
+const validateEmail = (emailString) => {
+  const emailRegEx =
+    /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+
+  const result = emailRegEx.test(emailString);
+
+  return result;
+};
+
+console.log(validateEmail("ddasd@aasda.com"));
+console.log(validateEmail("NotEmailAddress.com"));
