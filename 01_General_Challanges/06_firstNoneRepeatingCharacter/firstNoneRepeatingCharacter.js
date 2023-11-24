c1565d82718ce4cbbad05a25ec4067590123c51c
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-24 07:34:37 +0000

    add find non repeating character function

--- 01_General_Challanges/06_firstNoneRepeatingCharacter/firstNoneRepeatingCharacter.js
+++ 01_General_Challanges/06_firstNoneRepeatingCharacter/firstNoneRepeatingCharacter.js
@@ -2,4 +2,19 @@
 
 // * Using for loops
 
-const findFirstNoneRepeatingCharacter = () => {};
+const findFirstNoneRepeatingCharacter = (string) => {
+  const characterCount = {};
+
+  for (const character of string) {
+    characterCount[character] = (characterCount[character] || 0) + 1;
+  }
+  for (const character of string) {
+    if (characterCount[character] === 1) {
+      return character;
+    }
+  }
+
+  return null;
+};
+
+console.log(findFirstNoneRepeatingCharacter("aabbccdefg"));
