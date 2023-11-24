470ba49c325179b98fe2e7c9c6caf2af845620b0
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-24 07:55:11 +0000

    add dice game

--- 01_General_Challanges/07_diceGameSim/diceGameSim.js
+++ 01_General_Challanges/07_diceGameSim/diceGameSim.js
@@ -1,3 +1,31 @@
 /** @format */
 
-const diceGameSim = () => {};
+const roleDice = () => {
+  return Math.floor(Math.random() * 6) + 1;
+};
+
+const diceGameSim = (NumberOfTimes) => {
+  const results = [];
+
+  for (let index = 0; index < NumberOfTimes; index++) {
+    const roleDice_01 = roleDice();
+    const roleDice_02 = roleDice();
+    const sum = roleDice_01 + roleDice_02;
+
+    let result = "";
+
+    if (sum === 7 || sum === 11) {
+      result = "Win";
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = "Loss";
+    } else {
+      result = "Role again";
+    }
+
+    results.push({ roleDice_01, roleDice_02, sum, result });
+  }
+
+  return results;
+};
+
+console.log(diceGameSim(13));
