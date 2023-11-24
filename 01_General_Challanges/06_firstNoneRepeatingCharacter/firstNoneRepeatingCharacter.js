eb11b50129750ea7ab3c1d5b127b8c034cf1d87a
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-24 07:40:32 +0000

    add solution using maps

--- 01_General_Challanges/06_firstNoneRepeatingCharacter/firstNoneRepeatingCharacter.js
+++ 01_General_Challanges/06_firstNoneRepeatingCharacter/firstNoneRepeatingCharacter.js
@@ -18,3 +18,22 @@ const findFirstNoneRepeatingCharacter = (string) => {
 };
 
 console.log(findFirstNoneRepeatingCharacter("aabbccdefg"));
+
+// * Using map
+
+const findFirstNoneRepeatingCharacterUsingMap = (string) => {
+  const characterCount = new Map();
+
+  for (const character of string) {
+    characterCount.set(character, (characterCount.get(character) || 0) + 1);
+  }
+  for (const character of string) {
+    if (characterCount.get(character) === 1) {
+      return character;
+    }
+  }
+
+  return null;
+};
+
+console.log(findFirstNoneRepeatingCharacterUsingMap("aabbccdefg"));
