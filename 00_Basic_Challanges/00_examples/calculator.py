7708a1193b50e60824ac096aa1f56c5f0329a6db
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-26 08:52:50 +0000

    add calculate python code

---
+++ 00_Basic_Challanges/00_examples/calculator.py
@@ -0,0 +1,19 @@
+def calculate(a, b, operator):
+    """Calculate simple math operation on 2 numbers"""
+    first_number = a 
+    second_number = b
+    
+    if operator == "+":
+        result = first_number + second_number
+    elif operator == "-":
+        result = first_number - second_number
+    elif operator == "*":
+        result = first_number * second_number
+    elif operator == "/":
+        result = first_number / second_number
+    else:
+        raise ValueError("Invalid operator")
+        
+    return result
+
+print(calculate(10, 4, "+"))
