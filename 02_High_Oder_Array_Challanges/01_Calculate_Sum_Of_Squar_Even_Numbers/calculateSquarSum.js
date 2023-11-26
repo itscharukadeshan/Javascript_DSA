d0cf3a0b9d67a4fed52dff2052904552eab23d18
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-26 06:50:07 +0000

    add comment and console logs

--- 02_High_Oder_Array_Challanges/01_Calculate_Sum_Of_Squar_Even_Numbers/calculateSquarSum.js
+++ 02_High_Oder_Array_Challanges/01_Calculate_Sum_Of_Squar_Even_Numbers/calculateSquarSum.js
@@ -1,27 +1,39 @@
 /** @format */
 
-// * Generate Big number array
+// * Generate number array
 
 let numbers = [];
 
-for (let i = -1000; i <= 1000; i++) {
+for (let i = 0; i <= 10; i++) {
   numbers.push(i);
 }
 
 // * calculate sum using Filter , Map and Reduce
 
 const calculateSquareSum = (numberArray) => {
+  // ? Use filter method to filter the array to only contain even numbers (return new array)
+
   const evenNumberArray = numberArray.filter((number) => {
     return number % 2 === 0;
   });
+
+  // console.log(evenNumberArray);
+
+  // ? Use map to get square of all the even numbers (return new array)
 
   const squareOfEvenNumbers = evenNumberArray.map((number) => {
     return number * number;
   });
 
+  // console.log(squareOfEvenNumbers);
+
+  // ? Use reduce to calculate the sum of the square of the numbers (return the Sum)
+
   const sumOfArray = squareOfEvenNumbers.reduce((total, number) => {
     return total + number;
   }, 0);
+
+  // console.log(sumOfArray);
 
   const result = sumOfArray;
 
