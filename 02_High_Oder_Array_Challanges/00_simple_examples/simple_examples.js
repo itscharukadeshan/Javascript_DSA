4b4b45cd5b81fbc90d9f112e800f545cebc5c0ea
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-26 05:47:00 +0000

    add filter methods

--- 02_High_Oder_Array_Challanges/00_simple_examples/simple_examples.js
+++ 02_High_Oder_Array_Challanges/00_simple_examples/simple_examples.js
@@ -2,13 +2,23 @@
 
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 
+// * Map method
+
 const multipleByTwo = numbers.map((number, index, array) => {
   // ? Index give the index number of each number in array
-  console.log(index);
+  //console.log(index);
   //? Array gives access to the original input array
-  console.log(array);
+  // console.log(array);
   // ? You can do any operation to each individual number (in this case)
   return number * 2;
 });
 
-console.log(multipleByTwo);
+// console.log(multipleByTwo);
+
+// * Filter Method
+
+const oddNumbers = numbers.filter((number) => {
+  return number % 2 !== 0;
+});
+
+console.log(oddNumbers);
