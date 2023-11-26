4a7ea82a8cbcfcfed50c7822f2a9a022eee805e5
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-26 06:31:59 +0000

    add calculateSquareSum

--- 02_High_Oder_Array_Challanges/01_Calculate_Sum_Of_Squar_Even_Numbers/calculateSquarSum.js
+++ 02_High_Oder_Array_Challanges/01_Calculate_Sum_Of_Squar_Even_Numbers/calculateSquarSum.js
@@ -1,3 +1,21 @@
 /** @format */
 
-const calculateSquareSum = () => {};
+const calculateSquareSum = (numberArray) => {
+  const evenNumberArray = numberArray.filter((number) => {
+    return number % 2 === 0;
+  });
+
+  const squareOfEvenNumbers = evenNumberArray.map((number) => {
+    return number * number;
+  });
+
+  const sumOfArray = squareOfEvenNumbers.reduce((total, number) => {
+    return total + number;
+  }, 0);
+
+  const result = sumOfArray;
+
+  return result;
+};
+
+console.log(calculateSquareSum([1, 2, 3, 4, 5, 6, 7, 8]));
