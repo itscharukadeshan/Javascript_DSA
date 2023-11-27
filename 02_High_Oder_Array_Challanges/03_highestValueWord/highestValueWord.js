efd1cd96ae90d3b54ec648611fd281c5f6349a8d
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-27 08:05:33 +0000

    fix errors

--- 02_High_Oder_Array_Challanges/03_highestValueWord/highestValueWord.js
+++ 02_High_Oder_Array_Challanges/03_highestValueWord/highestValueWord.js
@@ -1,26 +1,30 @@
 /** @format */
 
-const string = "This is the String With XX";
+const string = "This is the String With highest value word => xxxx ";
 
 const highestValueWord = (string) => {
   const words = string.toLowerCase().split(" ");
 
-  let scores = 0;
+  let scores;
 
   const value = words.map((word) => {
+    scores = 0;
+
     for (const letter of word) {
       scores += letter.charCodeAt(0) - 96;
     }
+
     return scores;
   });
 
   let highestValue = 0;
+
   let highestIndex = 0;
 
   for (let i = 0; i < value.length; i++) {
-    if (scores[i] > highestValue) {
+    if (value[i] > highestValue) {
       highestIndex = i;
-      highestValue = scores[i];
+      highestValue = value[i];
     }
   }
 
