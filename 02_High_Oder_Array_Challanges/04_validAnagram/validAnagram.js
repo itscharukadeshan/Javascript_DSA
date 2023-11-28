957ba01892f2a3ee5eeda1c725b4153a10f3141e
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-28 10:27:19 +0000

    add valid anagram

--- 02_High_Oder_Array_Challanges/04_validAnagram/validAnagram.js
+++ 02_High_Oder_Array_Challanges/04_validAnagram/validAnagram.js
@@ -1,3 +1,25 @@
 /** @format */
 
-const validAnagram = () => {};
+const validAnagram = (string_01, string_02) => {
+  const frequencyCount_01 = string_01
+    .split("")
+    .reduce((accumulator, letter) => {
+      accumulator[letter] = (accumulator[letter] || 0) + 1;
+
+      return accumulator;
+    }, {});
+
+  const frequencyCount_02 = string_02
+    .split("")
+    .reduce((accumulator, letter) => {
+      accumulator[letter] = (accumulator[letter] || 0) + 1;
+
+      return accumulator;
+    }, {});
+
+  return Object.keys(frequencyCount_01).every((letter) => {
+    return frequencyCount_01[letter] === frequencyCount_02[letter];
+  });
+};
+
+console.log(validAnagram("app", "pap"));
