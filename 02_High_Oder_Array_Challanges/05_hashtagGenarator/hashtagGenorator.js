d36ce572735db1eab2070ce7d717dd22cc71fdfb
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-11-29 07:17:21 +0000

    add hashtag gen code

--- 02_High_Oder_Array_Challanges/05_hashtagGenarator/hashtagGenorator.js
+++ 02_High_Oder_Array_Challanges/05_hashtagGenarator/hashtagGenorator.js
@@ -1,3 +1,21 @@
 /** @format */
 
-const hashtagGenerator = () => {};
+// Using maps
+
+const hashtagGenerator = (string) => {
+  if (string.trim() === "") {
+    return false;
+  }
+
+  const words = string.trim().split(/\s+/);
+
+  const formattedWords = words.map((word) => {
+    return (word[0] = word[0].toUpperCase() + word.slice(1));
+  });
+
+  const hashTag = "#" + formattedWords.join("");
+
+  return hashTag.length > 140 ? false : hashTag;
+};
+
+console.log(hashtagGenerator("hello world"));
