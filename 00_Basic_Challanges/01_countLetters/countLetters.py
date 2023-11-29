ac87ecad952685f2cf38c3160a6af3224ffa8f1f
Charuka <itscharukadeshan@gmail.com>
2023-11-29 15:31:45 +0530

    fix the for loop

--- 00_Basic_Challanges/01_countLetters/countLetters.py
+++ 00_Basic_Challanges/01_countLetters/countLetters.py
@@ -1,18 +1,13 @@
-# ? Case inSensitive
+def countLetters(str, letter):
 
-def countLetters (str, letter):
-
-   count = 0
-   string = str.lower()
-   letterInLowercase = letter.lower()
-
-   for i in range(len(string)):
-        if string[i].lower() == letter:
+    count = 0
+    string = str.lower()
+    letter = letter.lower()
+    
+    for i in range(len(string)):
+        if string[i] == letter:
             count += 1
             
-            return f"Letter {letter} is present {count} times in {string}"
+    return f"Letter {letter} is present {count} times in {string}"
 
-
-
-    
 print(countLetters("My Name", "M"))
