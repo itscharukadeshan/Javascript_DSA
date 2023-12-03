0b8e70d6377c9590da91f940f964bf9b414ad1dd
Charuka <itscharukadeshan@gmail.com>
2023-12-03 10:47:42 +0530

    add analyzeCarMileage

--- 02_High_Oder_Array_Challanges/07_analizeCarMileage/analizeCarMileage.js
+++ 02_High_Oder_Array_Challanges/07_analizeCarMileage/analizeCarMileage.js
@@ -4,4 +4,24 @@ const analyzeCarMileage = (carArray) => {
   const totalMileage = carArray.reduce((sum, car) => {
     return sum + car.mileage;
   }, 0);
+
+  const averageMileage = totalMileage / carArray.length;
+
+  const highMileage = carArray.reduce((high, car) => {
+    return car.mileage > high.mileage ? car : high;
+  }, carArray[0]);
+
+  const lowestMileage = carArray.reduce((lowest, car) => {
+    return car.mileage < lowest.mileage ? car : lowest;
+  }, carArray[0]);
+
+  return {
+    AverageMileage: parseFloat(averageMileage.toFixed(2)),
+
+    HighMileageCar: highMileage,
+
+    LowestMileage: lowestMileage,
+
+    TotalMileage: totalMileage,
+  };
 };
