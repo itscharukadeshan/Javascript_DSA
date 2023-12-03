c4c35df464ac80605b07328bdb7d8a30150b984f
Charuka <itscharukadeshan@gmail.com>
2023-12-03 12:08:22 +0530

    add second way using filter

--- 02_High_Oder_Array_Challanges/09_findMissingLetter/findMissingLetter.js
+++ 02_High_Oder_Array_Challanges/09_findMissingLetter/findMissingLetter.js
@@ -21,3 +21,23 @@ const findMissingLetter = (array) => {
 };
 
 console.log(findMissingLetter(["a", "b", "c", "e"]));
+
+// * Using filter
+
+const findMissingLetterUsingFilter = (array) => {
+  const missingLetter = array.filter((char, index) => {
+    if (index == 0) return false;
+
+    const prevCharCode = array[index - 1].charCodeAt(0);
+
+    const CurrentCharCode = char.charCodeAt(0);
+
+    return CurrentCharCode - prevCharCode > 1;
+  })[0];
+
+  return missingLetter
+    ? String.fromCharCode(missingLetter.charCodeAt(0) - 1)
+    : "";
+};
+
+console.log(findMissingLetterUsingFilter(["a", "b", "c", "e"]));
