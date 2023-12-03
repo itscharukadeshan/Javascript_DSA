2763241772b70a577bf0320f365db112451b4f40
Charuka <itscharukadeshan@gmail.com>
2023-12-03 11:56:14 +0530

    add the missing letter using code

--- 02_High_Oder_Array_Challanges/09_findMissingLetter/findMissingLetter.js
+++ 02_High_Oder_Array_Challanges/09_findMissingLetter/findMissingLetter.js
@@ -1,3 +1,20 @@
 /** @format */
 
-const findMissingLetter = () => {};
+const findMissingLetter = (array) => {
+  let start = array[0].charCodeAt(0);
+
+  const missingLetter = array
+    .map((char) => char.charCodeAt(0))
+    .find((current) => {
+      if (current - 1 > start) {
+        return true;
+      }
+
+      start = current;
+      return false;
+    });
+
+  return missingLetter ? String.fromCharCode(missingLetter - 1) : "";
+};
+
+console.log(findMissingLetter(["a", "b", "c", "e"]));
