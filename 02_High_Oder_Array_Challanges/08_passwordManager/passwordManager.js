cc0bcce5f2e1be89931719131701432818cdf8e7
Charuka <itscharukadeshan@gmail.com>
2023-12-03 11:05:18 +0530

    add password check code

--- 02_High_Oder_Array_Challanges/08_passwordManager/passwordManager.js
+++ 02_High_Oder_Array_Challanges/08_passwordManager/passwordManager.js
@@ -1,3 +1,16 @@
 /** @format */
 
-const passwordManager = () => {};
+const passwordManager = (string) => {
+  const isValueCorrect = string.length >= 8;
+  const hasUpperCase = string
+    .split("")
+    .some((char) => char === char.toUpperCase() && char !== char.toLowerCase());
+  const hasLowerCase = string
+    .split("")
+    .some((char) => char === char.toLowerCase() && char !== char.toUpperCase());
+  const hasDigits = string.split("").some((char) => !isNaN(parseInt(char, 10)));
+
+  return isValueCorrect && hasUpperCase && hasLowerCase && hasDigits;
+};
+
+console.log(passwordManager("Abcd12345"));
