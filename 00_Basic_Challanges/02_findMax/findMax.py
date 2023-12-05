1d00469d4a3d5f650ee7caa3632c7e311e2c775b
Charuka <itscharukadeshan@gmail.com>
2023-12-05 12:08:55 +0530

    add findMaxWithForLoops

--- 00_Basic_Challanges/02_findMax/findMax.py
+++ 00_Basic_Challanges/02_findMax/findMax.py
@@ -8,3 +8,21 @@ def findMaxWithBuiltIn (array) :
 
 
 print(findMaxWithBuiltIn([1, 5, 3, 2, 7, 10, 5, 15]))
+
+
+## With for loops
+
+def findMaxWithForLoops(array):
+
+    max = array[0]
+    
+    for i in range(len(array)):
+
+        if array[i] > max:
+
+            max = array[i]
+
+    return max
+
+
+print(findMaxWithForLoops([1, 5, 3, 2, 7, 10, 5, 15]))
