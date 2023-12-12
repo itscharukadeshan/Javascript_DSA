74368fe5b439656025372e70bb9ca6c38ffa0c36
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-12 04:57:52 +0000

    docs📝:  Add comments to check recursively

--- 03_Recursion/01_unWinding/unWinding.js
+++ 03_Recursion/01_unWinding/unWinding.js
@@ -2,10 +2,17 @@
 
 const unWinding = (n) => {
   if (n === 1) {
+    console.log(`Base case reached with n=1. Returning 1`);
     return 1;
   }
 
-  return n + unWinding(n - 1);
+  console.log(`Calling unWinding(${n - 1}) recursively`);
+
+  const sum = n + unWinding(n - 1);
+
+  console.log(`Returning ${sum} back up the stack`);
+
+  return sum;
 };
 
-console.log(unWinding(6));
+console.log(unWinding(5));
