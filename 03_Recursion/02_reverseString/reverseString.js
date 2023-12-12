a10edd1aa7fa5910bef746863df860707ef973dd
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-12 05:17:14 +0000

    feat✨:  Add reverseString ( ) function

--- 03_Recursion/02_reverseString/reverseString.js
+++ 03_Recursion/02_reverseString/reverseString.js
@@ -1,3 +1,18 @@
 /** @format */
 
-const reverseString = () => {};
+const reverseString = (String) => {
+  // Base case
+
+  if (String === "") {
+    return "";
+  }
+
+  const newStr = String.substr(1);
+  const reversedRest = reverseString(newStr);
+
+  const result = reversedRest + String.charAt(0);
+
+  return result;
+};
+
+console.log(reverseString("hello"));
