5a153ef48c9be5fea8b62a8a4247365c15d71526
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-12 05:19:27 +0000

    docs📝: Add comment describing reverseString ( ) and unwinding

--- 03_Recursion/02_reverseString/reverseString.js
+++ 03_Recursion/02_reverseString/reverseString.js
@@ -3,14 +3,22 @@
 const reverseString = (String) => {
   // Base case
 
+  console.log(`Reverse function called with string: ${str}`);
+
   if (String === "") {
+    console.log(`Hit base case, empty string. Returning empty string`);
     return "";
   }
 
   const newStr = String.substr(1);
+
+  console.log(`Calling function recursively with: ${newStr}`);
+
   const reversedRest = reverseString(newStr);
 
   const result = reversedRest + String.charAt(0);
+
+  console.log(`Returning reversed string: ${result}`);
 
   return result;
 };
