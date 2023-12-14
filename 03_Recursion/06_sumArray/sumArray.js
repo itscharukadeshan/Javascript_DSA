66f5e6235b0bf253b20ec46227df441a9c70b003
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-14 07:52:18 +0000

    feat✨: Add sumArray ( ) function

--- 03_Recursion/06_sumArray/sumArray.js
+++ 03_Recursion/06_sumArray/sumArray.js
@@ -1,5 +1,11 @@
 /** @format */
 
-const sumArray = (array) => {};
+const sumArray = (array) => {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  return array[0] + sumArray(array.slice(1));
+};
 
 console.log(sumArray([1, 2, 3, 4, 5]));
