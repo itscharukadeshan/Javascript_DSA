999e9e06fb79842f8f17c276f30b0df114761185
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-14 07:31:27 +0000

    docs📝: Add console.log ( )  `Times factorial () called`

--- 03_Recursion/04_factorials/factorial.js
+++ 03_Recursion/04_factorials/factorial.js
@@ -1,11 +1,17 @@
 /** @format */
 
+let runtime = 0;
+
 const factorial = (number) => {
   if ((number === 1) | (number === 0)) {
     return 1;
   }
 
+  runtime++;
+
   return number * factorial(number - 1);
 };
 
-console.log(factorial(3));
+console.log(factorial(10));
+
+console.log(`Times factorial () called : ${runtime}  `);
