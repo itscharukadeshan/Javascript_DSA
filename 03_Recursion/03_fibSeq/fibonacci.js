d28fa7c39d56759d93e4372794ebf1514f7e3b07
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-14 06:48:25 +0000

    feat✨: Add fibonacci ( ) function

--- 03_Recursion/03_fibSeq/fibonacci.js
+++ 03_Recursion/03_fibSeq/fibonacci.js
@@ -1,3 +1,13 @@
 /** @format */
 
-const fibonacci = (params) => {};
+const fibonacci = (number) => {
+  console.log(`calculating ...fibonacci (${number}) `);
+
+  if (number < 2) {
+    return number;
+  }
+
+  return fibonacci(number - 1) + fibonacci(number - 2);
+};
+
+console.log(fibonacci(3));
