4b980ba64a35933769b327c515227a5c9e8721d9
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-14 06:56:12 +0000

    docs📝: Add times run console log and comment

--- 03_Recursion/03_fibSeq/fibonacci.js
+++ 03_Recursion/03_fibSeq/fibonacci.js
@@ -1,6 +1,11 @@
 /** @format */
 
+let runtime = 0;
+
+// * Take an number and return corresponding fib value at that Index
+
 const fibonacci = (number) => {
+  runtime++;
   console.log(`calculating ...fibonacci (${number}) `);
 
   if (number < 2) {
@@ -10,4 +15,6 @@ const fibonacci = (number) => {
   return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
-console.log(fibonacci(3));
+console.log(`Result : ${fibonacci(10)}`);
+
+console.log(`Times fibonacci called : ${runtime} `);
