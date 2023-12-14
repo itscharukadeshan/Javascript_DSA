5e6887b4c6a8d9d37687453316b53d4cfa2c6b5b
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-14 07:43:46 +0000

    feat✨: Add power ( ) function

--- 03_Recursion/05_power/power.js
+++ 03_Recursion/05_power/power.js
@@ -1,4 +1,16 @@
 /** @format */
 let runtime = 0;
 
-const power = (number) => {};
+const power = (base, exponent) => {
+  if (exponent === 0) {
+    return 1;
+  }
+
+  runtime++;
+
+  return base * power(base, exponent - 1);
+};
+
+console.log(power(2, 5));
+
+console.log(`Times power () called : ${runtime}`);
