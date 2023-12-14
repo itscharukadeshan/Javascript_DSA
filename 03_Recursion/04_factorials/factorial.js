12fc67c337be38701f3e5045fea83c904001bb4c
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-14 07:29:05 +0000

    feat✨: Add factorial ( ) function

--- 03_Recursion/04_factorials/factorial.js
+++ 03_Recursion/04_factorials/factorial.js
@@ -1,3 +1,11 @@
 /** @format */
 
-const factorial = (number) => {};
+const factorial = (number) => {
+  if ((number === 1) | (number === 0)) {
+    return 1;
+  }
+
+  return number * factorial(number - 1);
+};
+
+console.log(factorial(3));
