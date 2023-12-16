ac74478caab8846f7cc1c2b141dfe57f224ddffb
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-16 06:36:39 +0000

    feat✨: Add permutation ( ) function

--- 03_Recursion/09_permutation/permutation.js
+++ 03_Recursion/09_permutation/permutation.js
@@ -1,3 +1,26 @@
 /** @format */
 
-const permutation = () => {};
+const permutation = (string) => {
+  let result = [];
+  if (string.length === 0) {
+    result.push("");
+
+    return result;
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    const firstChar = string[i];
+
+    const restOfString = string.slice(0, i) + string.slice(i + 1);
+
+    const subPermutation = permutation(restOfString);
+
+    for (let j = 0; j < subPermutation.length; j++) {
+      result.push(firstChar + subPermutation[j]);
+    }
+  }
+
+  return result;
+};
+
+console.log(permutation("abc"));
