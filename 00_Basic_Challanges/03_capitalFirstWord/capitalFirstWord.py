56d325f0ff971aece6aff6f3b33e13bf68c42ee5
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-17 05:38:33 +0000

    feat✨:  Add capitalizeFirstWord : ( ) function

---
+++ 00_Basic_Challanges/03_capitalFirstWord/capitalFirstWord.py
@@ -0,0 +1,13 @@
+def capitalizeFirstWord(string):
+  
+  splitByWords = string.split()
+
+  for i in range(len(splitByWords)):
+
+    splitByWords[i] = splitByWords[i][0].upper() + splitByWords[i][1:]  
+
+  return " ".join(splitByWords)  
+
+
+print(capitalizeFirstWord("AA"))  
+print(capitalizeFirstWord("This is a sentence"))  
