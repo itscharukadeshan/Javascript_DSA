f8ce42cb6e7d316b1bd1665722d58aaf51fbe2ae
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-19 07:38:06 +0000

    docs📝: Add console.log ( ) and description

--- 04_Time_Complexity/01_linierTime/linierTime.js
+++ 04_Time_Complexity/01_linierTime/linierTime.js
@@ -1,3 +1,30 @@
 /** @format */
 
-const linierTime = () => {};
+const linierTime = (array) => {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
+};
+
+const firstArray = Array.from({ length: 10 }, (_, index) => index + 1);
+const secondArray = Array.from({ length: 100000 }, (_, index) => index + 1);
+
+// * With smaller input
+
+console.time("Access the fifth element | With smaller input ");
+
+console.log(linierTime(firstArray, 5));
+
+console.timeEnd("Access the fifth element | With smaller input ");
+
+// * With lager input
+
+console.time("Access the fifth element | With lager input ");
+
+console.log(linierTime(secondArray, 5));
+
+console.timeEnd("Access the fifth element | With lager input ");
