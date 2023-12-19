fe667b8397bddccbf90993dd929162a84b28017c
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-19 07:47:24 +0000

    refactor🎨: Increase the second input array

--- 04_Time_Complexity/01_linierTime/linierTime.js
+++ 04_Time_Complexity/01_linierTime/linierTime.js
@@ -10,10 +10,9 @@ const linierTime = (array) => {
   return sum;
 };
 
-const firstArray = [1, 2, 3, 4, 5, 6, 7];
-const secondArray = Array.from({ length: 100000 }, (_, index) => index + 1);
-
 // * With smaller input
+
+const firstArray = [1, 2, 3, 4, 5, 6, 7];
 
 console.time("Access the fifth element | With smaller input ");
 
@@ -22,6 +21,8 @@ console.log(linierTime(firstArray));
 console.timeEnd("Access the fifth element | With smaller input ");
 
 // * With lager input
+
+const secondArray = Array.from({ length: 100000000 }, (_, index) => index + 1);
 
 console.time("Access the fifth element | With lager input ");
 
