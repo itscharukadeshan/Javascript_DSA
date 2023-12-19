f5b1dc14cb7119212c6a2a3685f2493c2bf01584
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-19 07:22:18 +0000

    feat✨: Add constantTimeWithBigInput ( ) function

--- 04_Time_Complexity/00_constentTime/constantTime.js
+++ 04_Time_Complexity/00_constentTime/constantTime.js
@@ -4,6 +4,8 @@
 
 // ? Constant time is very hard to achieve in complex algorithm
 
+// Example 01 | Smaller Input
+
 const constantTime = (arr, index) => {
   return arr[index];
 };
@@ -11,5 +13,19 @@ const constantTime = (arr, index) => {
 console.time("Access the fifth element ");
 
 console.log(constantTime([1, 2, 3, 4, 5, 6], 5));
+
+console.timeEnd("Access the fifth element ");
+
+// Example 02 | Lager Input
+
+const BigArray = Array.from({ length: 1000 }, (_, index) => index + 1);
+
+const constantTimeWithBigInput = (arr, index) => {
+  return arr[index];
+};
+
+console.time("Access the fifth element ");
+
+console.log(constantTimeWithBigInput(BigArray, 5));
 
 console.timeEnd("Access the fifth element ");
