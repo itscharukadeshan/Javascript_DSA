73fccbb6412df263c94a82ca63b717b1bf3db2bd
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-19 07:08:27 +0000

    docs📝: Add comment and console.log ( )

--- 04_Time_Complexity/00_constentTime/constantTime.js
+++ 04_Time_Complexity/00_constentTime/constantTime.js
@@ -2,4 +2,10 @@
 
 // * Constant Time [ O (1) ]
 
-const constantTime = () => {};
+// ? Constant time is very hard to achieve in complex algorithm
+
+const constantTime = (arr, index) => {
+  return arr[index];
+};
+
+console.log(constantTime([1, 2, 3, 4, 5, 6], 5));
