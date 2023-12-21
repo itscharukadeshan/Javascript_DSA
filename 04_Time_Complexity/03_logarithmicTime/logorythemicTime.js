3414a33a13ffdd39e7f359e039b7a57be369db23
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-21 07:23:51 +0000

    feat✨:  Add findPower ( ) function

--- 04_Time_Complexity/03_logarithmicTime/logorythemicTime.js
+++ 04_Time_Complexity/03_logarithmicTime/logorythemicTime.js
@@ -1,3 +1,15 @@
 /** @format */
 
-const logarithmicTime = (base, exponent) => {};
+const findPower = (base, exponent) => {
+  if (exponent === 0) {
+    return 1;
+  }
+
+  if (exponent % 2 === 0) {
+    const halfPower = findPower(base, exponent / 2);
+    return halfPower * halfPower;
+  } else {
+    const halfPower = findPower(base, (exponent - 1) / 2);
+    return halfPower * halfPower;
+  }
+};
