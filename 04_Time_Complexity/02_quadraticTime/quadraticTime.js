fc116b1be43d0f9b84c1391bc0a5eda1c205cf20
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-21 07:07:25 +0000

    feat✨:  Add quadraticTime ( ) function

--- 04_Time_Complexity/02_quadraticTime/quadraticTime.js
+++ 04_Time_Complexity/02_quadraticTime/quadraticTime.js
@@ -1,3 +1,16 @@
 /** @format */
 
-const quadraticTime = () => {};
+const quadraticTime = (array) => {
+  let sum = 0;
+  let sum_02 = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+
+    for (let j = 0; j < array.length; j++) {
+      sum += array[j];
+    }
+  }
+
+  return sum + sum_02;
+};
