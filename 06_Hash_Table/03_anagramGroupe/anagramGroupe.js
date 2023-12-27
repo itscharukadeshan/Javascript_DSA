5ed4936d7246d9500d7f916c458a367e125e2ac9
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-27 05:33:43 +0000

    feat✨: Add anagramGroupe ( ) function

--- 06_Hash_Table/03_anagramGroupe/anagramGroupe.js
+++ 06_Hash_Table/03_anagramGroupe/anagramGroupe.js
@@ -1,3 +1,17 @@
 /** @format */
 
-const anagramGroupe = (Array) => {};
+const anagramGroupe = (array) => {
+  const anagramGroups = new Map();
+
+  for (const word of array) {
+    const shortedChar = word.split("").sort().join("");
+    if (anagramGroups.has(shortedChar)) {
+      anagramGroups.get(shortedChar).push(word);
+    } else {
+      anagramGroups.set(shortedChar, [word]);
+    }
+  }
+  return Array.from(anagramGroups.values());
+};
+
+console.log(anagramGroupe(["cat", "act", "dog", "god", "nonAnagram"]));
