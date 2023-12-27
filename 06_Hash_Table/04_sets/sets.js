504468bb76c9202f4586bf13734a74fc64e82da3
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-27 05:48:56 +0000

    feat✨: Add set method examples

--- 06_Hash_Table/04_sets/sets.js
+++ 06_Hash_Table/04_sets/sets.js
@@ -5,3 +5,34 @@ const newSet = new Set(["one", "two", "three", "one"]);
 // * Set only take unique values
 
 console.log(newSet);
+
+// * Adding new values
+
+newSet.add("four");
+
+console.log(newSet);
+
+// Checking for values
+
+newSet.has("one");
+newSet.has("six");
+
+// * Size of the Set
+
+console.log(newSet.size);
+
+// * get values
+
+console.log(newSet.values());
+
+// With for loop
+
+for (const number of newSet) {
+  console.log(number);
+}
+
+// * Convert to an array
+
+const newArray = [...newSet];
+
+console.log(newArray);
