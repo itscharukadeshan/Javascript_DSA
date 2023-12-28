3389452568b800456193d8ec72f7d9bd859e92a4
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-28 05:13:20 +0000

    feat✨: Add uniqNumbers ( ) function

--- 06_Hash_Table/05_uniqNumbers/uniqNumbers.js
+++ 06_Hash_Table/05_uniqNumbers/uniqNumbers.js
@@ -1,3 +1,24 @@
 /** @format */
 
-const uniqNumbers = (array_1, array_2) => {};
+const uniqNumbers = (array_1, array_2) => {
+  const set_01 = new Set(array_1);
+  const set_02 = new Set(array_2);
+
+  const result = [];
+
+  for (const number of set_01) {
+    if (!set_02.has(number)) {
+      result.push(number);
+    }
+  }
+
+  for (const number of set_02) {
+    if (!set_01.has(number)) {
+      result.push(number);
+    }
+  }
+
+  return result;
+};
+
+console.log(uniqNumbers([1, 2, 3, 4, 5, 6, 7], [1, 5, 6, 7, 8, 9, 10]));
