98fc366dcd25de01c501b5a0c72032e47c6eea59
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-28 05:29:58 +0000

    feat✨: Add sumTwo ( ) function

--- 06_Hash_Table/06_SumTwo/sumTwo.js
+++ 06_Hash_Table/06_SumTwo/sumTwo.js
@@ -1,3 +1,17 @@
 /** @format */
 
-const sumTwo = () => {};
+const sumTwo = (numberArray, target) => {
+  const numSet = new Set();
+  for (let i = 0; i < numberArray.length; i++) {
+    const compliment = target - numberArray[i];
+
+    if (numSet.has(compliment)) {
+      return [numberArray.indexOf(compliment), i];
+    }
+    numSet.add(numberArray[i]);
+  }
+
+  return [];
+};
+
+console.log(sumTwo([1, 2, 3], 5));
