4d18f2a8f04b868a97c5a3712ff1ce3e738f20de
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-30 06:35:06 +0000

    feat✨: Add CustomHashTable class and create newHashTable

--- 06_Hash_Table/08_customHashTable/customHashTable.js
+++ 06_Hash_Table/08_customHashTable/customHashTable.js
@@ -1,10 +1,22 @@
 /** @format */
 
-class customHashTable {
+class CustomHashTable {
   constructor(limit = 14) {
     this.storage = [];
     this.limit = limit;
   }
 
-  _hash(key, max) {}
+  _hash(key, max) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+
+    return hash % max;
+  }
 }
+
+const newHashTable = new CustomHashTable();
+const result = newHashTable._hash("myKey", newHashTable.limit);
+
+console.log(result);
