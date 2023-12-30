08c30d2e68388fc7be02827ccfff21c6bcccc79a
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2023-12-30 06:19:29 +0000

    feat✨: Add longestSequence ( ) function

--- 06_Hash_Table/07_longestSequence/longestSequence.js
+++ 06_Hash_Table/07_longestSequence/longestSequence.js
@@ -1,3 +1,22 @@
 /** @format */
 
-const longestSequence = () => {};
+const longestSequence = (array) => {
+  const numSet = new Set(array);
+  let longestSeq = 0;
+
+  for (const num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentSeq = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        currentSeq++;
+      }
+      longestSeq = Math.max(longestSeq, currentSeq);
+    }
+  }
+  return longestSeq;
+};
+
+console.log(longestSequence([100, 4, 200, 1, 2, 3]));
