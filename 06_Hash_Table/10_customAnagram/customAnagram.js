d65a3736aecfea8bcb6ed148ea918d4f081126fb
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-02 06:11:17 +0000

    feat✨: Add customAnagram ( ) function

--- 06_Hash_Table/10_customAnagram/customAnagram.js
+++ 06_Hash_Table/10_customAnagram/customAnagram.js
@@ -2,4 +2,16 @@
 
 const HashTable = require("../08_customHashTable/customHashTable");
 
-const customAnagram = (words) => {};
+const customAnagram = (words) => {
+  const anagramGrouping = new HashTable();
+  for (const word of words) {
+    const shortedChar = word.split("").sort().join("");
+
+    if (anagramGrouping.get(shortedChar)) {
+      anagramGrouping.get(shortedChar).push(word);
+    } else {
+      anagramGrouping.set(shortedChar, [word]);
+    }
+  }
+  return anagramGrouping.getValue();
+};
