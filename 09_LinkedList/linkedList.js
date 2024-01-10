3e97a2ee3b827d1d2cf5d2dd4777eb5a8fed1592
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-10 05:25:28 +0000

    feat✨: Add LinkedList class

---
+++ 09_LinkedList/linkedList.js
@@ -0,0 +1,35 @@
+/** @format */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(data) {
+    const node = new Node(data);
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  }
+
+  printAll() {
+    let current = this.head;
+
+    while (current !== null) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
