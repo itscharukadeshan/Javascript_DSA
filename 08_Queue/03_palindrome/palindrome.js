0af8c614a19cb165d59fb000a281d920d9a11dcd
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-10 05:09:12 +0000

    feat✨: Add palindrome ( ) function

--- 08_Queue/03_palindrome/palindrome.js
+++ 08_Queue/03_palindrome/palindrome.js
@@ -3,4 +3,25 @@
 const Stack = require("../../07_Stacks/01_stackExample/stack");
 const Queue = require("../01_queue/queue");
 
-const palindrome = (string) => {};
+const palindrome = (string) => {
+  formattedStr = string.replace(/[^a-zA-Z0-1]/g, "").toLowerCase();
+  const chatStack = new Stack();
+  const charQueue = new Queue();
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    const char = formattedStr.charAt(i);
+
+    charQueue.enqueue(char);
+    chatStack.push(char);
+  }
+  while (!charQueue.isEmpty()) {
+    if (charQueue.dequeue() !== chatStack.pop()) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(palindrome("a Man , a Plan , a canal:panama"));
+
+console.log(palindrome("Hello !"));
