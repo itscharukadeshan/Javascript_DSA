14c10a60e2f239d396009d6893925c497e7e2f1f
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-13 07:55:22 +0000

    feat✨: Add insertAt method to handle insert data in specific index

--- 09_LinkedList/linkedList.js
+++ 09_LinkedList/linkedList.js
@@ -47,6 +47,29 @@ class LinkedList {
       current = current.next;
     }
   }
+
+  insertAt(index, data) {
+    if (index === 0) {
+      const node = new Node(data);
+
+      node.next = this.head;
+      this.head = node;
+    } else {
+      let current = this.head;
+      let prev = 0;
+      let i = 0;
+
+      while (i < index) {
+        prev = current;
+        current = current.next;
+        i++;
+      }
+
+      const node = new Node(data);
+      node.next = current;
+      prev.next = node;
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
