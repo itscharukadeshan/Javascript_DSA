60f5161e9e21831d0e6d2678441cb62179e3e2f5
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-13 08:25:49 +0000

    feat✨: Add reverseString ( ) function

--- 09_LinkedList/01_reverseString/reverseString.js
+++ 09_LinkedList/01_reverseString/reverseString.js
@@ -1,4 +1,21 @@
 /** @format */
 const { LinkedList } = require("../linkedList");
-const list = new LinkedList();
-const reverseString = () => {};
+
+const reverseString = (string) => {
+  const list = new LinkedList();
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    list.add(string[i]);
+  }
+
+  let reverseString = "";
+  let current = list.head;
+
+  while (current !== null) {
+    reverseString += current.data;
+    current = current.next;
+  }
+  return reverseString;
+};
+
+console.log(reverseString("! dlrow olleH"));
