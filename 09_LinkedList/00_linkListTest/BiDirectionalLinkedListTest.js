fd58f7f5472e2e4043b0577ea5f120e6c788b208
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-14 07:12:11 +0000

    feat✨: add BiDirectionalLinkedListTest examples

--- 09_LinkedList/00_linkListTest/BiDirectionalLinkedListTest.js
+++ 09_LinkedList/00_linkListTest/BiDirectionalLinkedListTest.js
@@ -1,3 +1,13 @@
 /** @format */
 
-const BiDirectionalLinkedList = require("../BiDirectionalLinkedList");
+const { BiDirectionalLinkedList } = require("../BiDirectionalLinkedList");
+
+const list = new BiDirectionalLinkedList();
+
+list.append(100);
+list.append(200);
+list.append(300);
+list.append(400);
+list.append(400);
+
+list.printAll();
