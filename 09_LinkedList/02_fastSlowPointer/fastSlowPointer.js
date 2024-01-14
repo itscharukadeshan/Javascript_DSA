04f5ce1e16aee0f74602684e1b94db5578e98c3c
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-14 06:41:55 +0000

    feat✨: Add fastSlowPointer ( ) function

--- 09_LinkedList/02_fastSlowPointer/fastSlowPointer.js
+++ 09_LinkedList/02_fastSlowPointer/fastSlowPointer.js
@@ -2,4 +2,30 @@
 
 const { LinkedList } = require("../linkedList");
 
-const fastSlowPointer = () => {};
+const fastSlowPointer = (list) => {
+  let slow = list.head;
+  let fast = list.head;
+  let prev = null;
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    prev = slow;
+    slow = slow.next;
+  }
+
+  if (fast === null) {
+    return prev.next;
+  } else {
+    return slow;
+  }
+};
+
+const list = new LinkedList();
+
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+list.add(5);
+
+console.log(fastSlowPointer(list));
