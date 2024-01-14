3c5a3e5c525af0005819c07d5a1e9a1013dcbd16
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-14 07:07:22 +0000

    feat✨: Add Node and BiDirectionalLinkedList classes

---
+++ 09_LinkedList/BiDirectionalLinkedList.js
@@ -0,0 +1,40 @@
+/** @format */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+}
+class BiDirectionalLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(data) {
+    const newNode = new Node();
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  printAll() {
+    let current = this.head;
+
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+module.exports = { Node, BiDirectionalLinkedList };
