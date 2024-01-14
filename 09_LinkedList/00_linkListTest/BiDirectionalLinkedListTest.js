204279341baaa97c3533ad044288038fe08eb272
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-14 07:39:07 +0000

    docs📝: Simplify the the examples to easy to understand

--- 09_LinkedList/00_linkListTest/BiDirectionalLinkedListTest.js
+++ 09_LinkedList/00_linkListTest/BiDirectionalLinkedListTest.js
@@ -4,22 +4,20 @@ const { BiDirectionalLinkedList } = require("../BiDirectionalLinkedList");
 
 const list = new BiDirectionalLinkedList();
 
+// * Append items to the list
+
 list.append(100);
 list.append(200);
 list.append(300);
 list.append(400);
 list.append(400);
 
-list.printAll();
-
 // * adding new node before 100
 
-list.prePend(50);
-
-list.printAll();
+list.prePend("=> 50");
 
 // adding new node in between index 0 - 1 (50 -[>=newNodeHere<=] -100)
 
-list.insertAt(1, 70);
+list.insertAt(1, ">= 70 <=");
 
 list.printAll();
