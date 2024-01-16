4a8c7aeb3d64f007cb5cc869e0eeb3347b27036a
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-16 06:37:55 +0000

    feat✨: Add pairSum ( )  function

--- 09_LinkedList/03_pairSum/pairSum.js
+++ 09_LinkedList/03_pairSum/pairSum.js
@@ -1,5 +1,21 @@
 /** @format */
 
-const { LinkedList } = require("../linkedList");
+const { BiDirectionalLinkedList } = require("../BiDirectionalLinkedList");
 
-const pairSum = () => {};
+const pairSum = (numbers, targetNumber) => {
+  const seen = new BiDirectionalLinkedList();
+
+  for (const num of numbers) {
+    const difference = targetNumber - num;
+
+    if (seen.contain(difference)) {
+      return [difference, num];
+    }
+    seen.append(num);
+  }
+
+  return null;
+};
+
+console.log(pairSum([1, 2, 3, 4, 5, 6, 7], 13));
+console.log(pairSum([1, 2, 3, 4, 5, 6, 7], 100));
