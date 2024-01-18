c1e08517a81f24c4268067c738d6b0b98a185cb9
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-18 13:06:36 +0000

    feat✨: Add Tree implementation

--- 10_BinaryTree/treeNode.js
+++ 10_BinaryTree/treeNode.js
@@ -8,3 +8,12 @@ const c = new TreeNode("c");
 const d = new TreeNode("d");
 const e = new TreeNode("e");
 const f = new TreeNode("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+
+c.right = f;
+
+console.log(a);
