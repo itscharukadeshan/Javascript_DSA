f6d907296fe0aa7aa030723ba43c8f98ed5e5023
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-18 13:40:21 +0000

    file📦: Add resultWithStack ( ) function

--- 10_BinaryTree/depthFirstTraversal/depthFirstTraversals.js
+++ 10_BinaryTree/depthFirstTraversal/depthFirstTraversals.js
@@ -10,6 +10,8 @@ class Node {
   }
 }
 
+// * Without stack
+
 const depthFirstTraversal = (root) => {
   if (!root) {
     return [];
@@ -35,7 +37,35 @@ const depthFirstTraversal = (root) => {
   return result;
 };
 
+// * With the stack
+
+const depthFirstTraversalWithStack = (root) => {
+  if (!root) {
+    return [];
+  }
+
+  const result = [];
+  const stack = new Stack();
+
+  stack.push(root);
+
+  while (!stack.isEmpty()) {
+    const current = stack.pop();
+    result.push(current.value);
+
+    if (current.right) {
+      stack.push(current.right);
+    }
+    if (current.left) {
+      stack.push(current.left);
+    }
+  }
+
+  return result;
+};
+
 module.exports = {
   Node,
   depthFirstTraversal,
+  depthFirstTraversalWithStack,
 };
