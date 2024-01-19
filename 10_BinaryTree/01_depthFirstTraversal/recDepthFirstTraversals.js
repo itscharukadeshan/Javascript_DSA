ca8a875eb282c9bc97e98e1356a1c237485dc3c7
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-19 05:48:24 +0000

    refactor🎨: reorder the files and add new recDepthFirstTraversals.js

---
+++ 10_BinaryTree/01_depthFirstTraversal/recDepthFirstTraversals.js
@@ -0,0 +1,13 @@
+/** @format */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+const recDepthFirstTraversals = () => {};
+
+module.exports = { Node, recDepthFirstTraversals };
