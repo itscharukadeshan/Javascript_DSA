77053ea3ec4d4361165a6489b42ee8153bfbcda6
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-20 05:51:39 +0000

    file📦: Add Node.js and maxDepth.js files

---
+++ 10_BinaryTree/03_maxDepth/maxDepth.js
@@ -0,0 +1,13 @@
+/** @format */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const maxDepth = (root) => {};
+
+module.exports = { Node, maxDepth };
---
+++ 10_BinaryTree/03_maxDepth/test.js
@@ -0,0 +1,3 @@
+/** @format */
+
+const { Node, maxDepth } = require("./maxDepth");
