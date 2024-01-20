918f6db4491f5a9157cbf89f5fb8c98105025f60
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-20 06:00:33 +0000

    docs📝: Add data to the tree and example for maxDepth

--- 10_BinaryTree/03_maxDepth/test.js
+++ 10_BinaryTree/03_maxDepth/test.js
@@ -1,3 +1,18 @@
 /** @format */
 
 const { Node, maxDepth } = require("./maxDepth");
+
+const root = new Node(3);
+const nodeB = new Node(9);
+const nodeC = new Node(20);
+const nodeD = new Node(15);
+const nodeE = new Node(7);
+
+root.right = nodeB;
+root.left = nodeC;
+nodeC.right = nodeE;
+nodeC.left = nodeD;
+
+const result = maxDepth(root);
+
+console.log(result);
