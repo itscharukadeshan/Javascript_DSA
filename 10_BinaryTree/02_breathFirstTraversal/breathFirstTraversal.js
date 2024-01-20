5edd4334806f4f00b25c231315963927d8ee96db
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-20 05:31:44 +0000

    file📦: Add breathFirstTraversal.js

---
+++ 10_BinaryTree/02_breathFirstTraversal/breathFirstTraversal.js
@@ -0,0 +1,15 @@
+/** @format */
+
+const Queue = require("./queue");
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const breathFirstTraversal = () => {};
+
+module.exports = { Node, breathFirstTraversal };
