04b24f8e67b7047f44663450509bdab2cc384bcd
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-20 05:37:15 +0000

    file📦: Add test.js file

---
+++ 10_BinaryTree/02_breathFirstTraversal/test.js
@@ -0,0 +1,20 @@
+/** @format */
+
+const { Node, breathFirstTraversal } = require("./breathFirstTraversal");
+
+const root = new Node("a");
+const nodeB = new Node("b");
+const nodeC = new Node("c");
+const nodeD = new Node("d");
+const nodeE = new Node("e");
+const nodeF = new Node("f");
+
+root.right = nodeB;
+root.left = nodeC;
+nodeB.right = nodeD;
+nodeB.left = nodeE;
+nodeC.left = nodeF;
+
+const result = breathFirstTraversal(root);
+
+console.log(result);
