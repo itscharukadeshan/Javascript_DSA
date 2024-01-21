381ff8cf8612ee9c24c87aa11529c4ca61612154
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-21 07:34:09 +0000

    docs📝: Add  printTreeStructure examples and explanation how biTree works

--- 11_BinarySearchTree/test.js
+++ 11_BinarySearchTree/test.js
@@ -4,10 +4,31 @@ const { Node, BiSearchTree } = require("./binarySearchTree");
 
 const bst = new BiSearchTree();
 
+bst.insert(20);
 bst.insert(10);
-bst.insert(12);
+bst.insert(5);
+bst.insert(15);
 bst.insert(30);
+bst.insert(25);
+bst.insert(35);
 
-bst.insert(85);
+bst.printTreeStructure();
 
-bst.printTree();
+// ? Parent only can have two (binary) children
+// ? Parent.right is lager than parent
+// ? Parent.left is smaller that parent
+
+// * Output
+
+/*
+
+
+│       ┌── 35 R
+│   ┌── 30 R
+│   │   └── 25 L
+└── 20 L
+    │   ┌── 15 R
+    └── 10 L
+        └── 5 L
+
+ */
