67d5a111686cf0647a86c6e43ed9c2112950a336
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-21 07:06:59 +0000

    docs📝: Add bst example

--- 11_BinarySearchTree/test.js
+++ 11_BinarySearchTree/test.js
@@ -1,3 +1,13 @@
 /** @format */
 
 const { Node, BiSearchTree } = require("./binarySearchTree");
+
+const bst = new BiSearchTree();
+
+bst.insert(10);
+bst.insert(12);
+bst.insert(30);
+
+bst.insert(85);
+
+bst.printTree();
