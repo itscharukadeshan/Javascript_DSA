b6c7f4be67a987328e5f6f67d8d7a291871f7e03
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-21 06:42:28 +0000

    file📦: Add BiSearchTree.js

---
+++ 11_BinarySearchTree/binarySearchTree.js
@@ -0,0 +1,15 @@
+/** @format */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class BiSearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
