6c2f34a8d30442871b346ae1e094785a44d4e979
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-23 08:16:17 +0000

    file📦: Add validateBST file

---
+++ 11_BinarySearchTree/validateBST.js
@@ -0,0 +1,13 @@
+/** @format */
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const isValidateBST = () => {};
+
+module.exports = { Node, isValidateBST };
