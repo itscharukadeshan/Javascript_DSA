6568c250c0d4a3ff8c0fc4cd2914b9327e1ced88
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-23 08:24:47 +0000

    feat✨: Add isValidateBST ( ) function

--- 11_BinarySearchTree/validateBST.js
+++ 11_BinarySearchTree/validateBST.js
@@ -8,6 +8,24 @@ class Node {
   }
 }
 
-const isValidateBST = () => {};
+const isValidateBST = (root) => {
+  const isValidSubTree = (node, min, max) => {
+    if (!node) return true;
+
+    if (
+      (min !== null && node.value <= min) ||
+      (max !== null && node.value >= max)
+    ) {
+      return false;
+    }
+
+    return (
+      isValidSubTree(node.left, min, node.value) &&
+      isValidSubTree(node.right, max, node.value)
+    );
+  };
+
+  return isValidSubTree(root, null, null);
+};
 
 module.exports = { Node, isValidateBST };
