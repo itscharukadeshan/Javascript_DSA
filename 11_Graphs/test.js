3c9191f71b262d1a4bdf6e3efd79ef6abe187be8
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-24 08:33:38 +0000

    docs📝: Add removeEdge method example and update the graph example

--- 11_Graphs/test.js
+++ 11_Graphs/test.js
@@ -3,9 +3,29 @@
 const Graph = require("./graph");
 
 const graph = new Graph();
-graph.addVertex("A");
-graph.addVertex("B");
-graph.addVertex("C");
-graph.addEdge("A", "B");
-graph.addEdge("A", "C");
+
+graph.addVertex("Shark");
+graph.addVertex("Octopus");
+graph.addVertex("Dolphin");
+graph.addVertex("Seal");
+graph.addVertex("Turtle");
+graph.addVertex("Whale");
+
+graph.addEdge("Shark", "Octopus");
+graph.addEdge("Shark", "Dolphin");
+graph.addEdge("Shark", "Turtle");
+graph.addEdge("Octopus", "Dolphin");
+graph.addEdge("Seal", "Whale");
+graph.addEdge("Dolphin", "Whale");
+
 graph.printAdjacencyList();
+
+// ---------------------
+
+// * Remove edge
+
+graph.removeEdge("Octopus", "Dolphin");
+
+graph.printAdjacencyList();
+
+// ? remove Octopus and Dolphin connections
