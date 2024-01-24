844b68f475074458151d4a6d5f33faf1eb47edb5
Charuka Deshan <104638057+itscharukadeshan@users.noreply.github.com>
2024-01-24 08:08:43 +0000

    feat✨: Add addVertex,addEdge and printAdjacencyList methods

--- 11_Graphs/graph.js
+++ 11_Graphs/graph.js
@@ -1,5 +1,25 @@
 /** @format */
 
-class Graph {}
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(node_1, node_2) {
+    this.adjacencyList[node_1].push(node_2);
+    this.adjacencyList[node_2].push(node_1);
+  }
+  printAdjacencyList() {
+    for (let vertex in this.adjacencyList) {
+      let edges = this.adjacencyList[vertex];
+
+      console.log(`${vertex} --> ${edges} `);
+    }
+  }
+}
 
 module.exports = Graph;
---
+++ 11_Graphs/test.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const Graph = require("./graph");
+
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.printAdjacencyList();
