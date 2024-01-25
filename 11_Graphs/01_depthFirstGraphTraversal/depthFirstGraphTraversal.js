dcd2a6b631d1a8cf9cc4e5061dff881a30edb67c
Charuka Deshan <itscharukadeshan@gmail.com>
2024-01-25 13:04:54 +0530

    feat✨: Add depthFirstTraversal ( ) function

--- 11_Graphs/01_depthFirstGraphTraversal/depthFirstGraphTraversal.js
+++ 11_Graphs/01_depthFirstGraphTraversal/depthFirstGraphTraversal.js
@@ -1,3 +1,29 @@
 /** @format */
+const Stack = require("./stack");
 
-const depthFirstTraversal = (graph, startingNode) => {};
+const depthFirstTraversal = (graph, startingNode) => {
+  if (!graph.adjacencyList[startingNode]) {
+    return [];
+  }
+  const visited = {};
+  const stack = new Stack();
+  const result = [];
+
+  stack.push(startingNode);
+  visited[startingNode] = true;
+
+  while (!stack.isEmpty()) {
+    const currentNode = stack.pop();
+    result.push(currentNode);
+
+    graph.adjacencyList[currentNode].forEach((neighbor) => {
+      if (!visited[neighbor]) {
+        visited[neighbor] = true;
+        stack.push(neighbor);
+      }
+    });
+  }
+  return result;
+};
+
+module.exports = depthFirstTraversal;
