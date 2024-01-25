979bc2bb317b1524ff0c0e6d2877ef26babf6da3
Charuka Deshan <itscharukadeshan@gmail.com>
2024-01-25 13:26:31 +0530

    docs📝: Add the breathFirstTraversal ( ) function

--- 11_Graphs/02_breathFirstTraversal/breathFirstTraversal.js
+++ 11_Graphs/02_breathFirstTraversal/breathFirstTraversal.js
@@ -2,6 +2,26 @@
 
 const Queue = require("../queue");
 
-const breathFirstTraversal = () => {};
+const breathFirstTraversal = (graph, startingNode) => {
+  const visited = new Set();
+  const result = [];
+  const queue = new Queue();
+
+  queue.enqueue(startingNode);
+  visited.add(startingNode);
+
+  while (!queue.isEmpty()) {
+    const currentNode = queue.dequeue();
+    result.push(currentNode);
+
+    for (const neighbor of graph.adjacencyList[currentNode]) {
+      if (!visited.has(neighbor)) {
+        queue.enqueue(neighbor);
+        visited.add(neighbor);
+      }
+    }
+  }
+  return result;
+};
 
 module.exports = breathFirstTraversal;
