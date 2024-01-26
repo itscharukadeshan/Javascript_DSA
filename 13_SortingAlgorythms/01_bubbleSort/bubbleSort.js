e9e75fbf411cc7776c8af8831fa78121310ad369
Charuka Deshan <itscharukadeshan@gmail.com>
2024-01-26 15:29:36 +0530

    feat✨: Add bubbleSort ( ) function

--- 13_SortingAlgorythms/01_bubbleSort/bubbleSort.js
+++ 13_SortingAlgorythms/01_bubbleSort/bubbleSort.js
@@ -1,5 +1,18 @@
 /** @format */
 
-const bubbleSort = () => {};
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
 
-modules.export = bubbleSort;
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+};
+
+module.exports = bubbleSort;
