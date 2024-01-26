1fedf31c8ba70f2d30bb528d1134c71ff5cad802
Charuka Deshan <itscharukadeshan@gmail.com>
2024-01-26 16:01:03 +0530

    docs📝: Add examples for bubbleSort and bubbleSort_2

--- 13_SortingAlgorythms/01_bubbleSort/test.js
+++ 13_SortingAlgorythms/01_bubbleSort/test.js
@@ -1,9 +1,21 @@
 /** @format */
 
-const bubbleSort = require("./bubbleSort.js");
+const { bubbleSort, bubbleSort_2 } = require("./bubbleSort.js");
 
 const array = [5, 4, 2, 1];
 
+// _____________________________________
+
 const result = bubbleSort(array);
 
-console.log(result);
+console.log(" ".repeat(10));
+
+console.log(`result => ${result}`);
+
+console.log(" ".repeat(10));
+
+// _____________________________________
+
+const result_02 = bubbleSort_2(array);
+
+console.log(`result_02 => ${result_02}`);
