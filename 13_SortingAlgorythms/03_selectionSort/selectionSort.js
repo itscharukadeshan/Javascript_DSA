9c786d8863877c2d6441085e2043bf6efbace553
Charuka <itscharukadeshan@gmail.com>
2024-02-02 15:18:27 +0530

    feat✨: Add selectionSort ( ) function

--- 13_SortingAlgorythms/03_selectionSort/selectionSort.js
+++ 13_SortingAlgorythms/03_selectionSort/selectionSort.js
@@ -1,5 +1,21 @@
 /** @format */
 
-const selectionSort = () => {};
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [array[i], array[minIndex]] = [array[minIndex], array[i]];
+    }
+  }
+
+  return array;
+};
 
 module.exports = { selectionSort };
