06749cca34827fbec3c8954fc84684cc6b0f6d8a
Charuka <itscharukadeshan@gmail.com>
2024-02-02 15:39:36 +0530

    file📦: Add test.js

---
+++ 13_SortingAlgorythms/05_quickSort/tets.js
@@ -0,0 +1,9 @@
+/** @format */
+
+const { quickSort } = require("./quickSort");
+
+const unSortedArray = [4, 3, 2, 10, 12, 1, 5, 6];
+
+const result = quickSort(unSortedArray);
+
+console.log(result);
