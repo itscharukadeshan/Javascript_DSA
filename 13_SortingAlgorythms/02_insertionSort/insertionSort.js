c5918acaf60da3363acc0741f12cf2bc1ce72dd3
Charuka <itscharukadeshan@gmail.com>
2024-02-02 15:02:41 +0530

    feat✨: Add insertionSort ( ) function

--- 13_SortingAlgorythms/02_insertionSort/insertionSort.js
+++ 13_SortingAlgorythms/02_insertionSort/insertionSort.js
@@ -1,5 +1,20 @@
 /** @format */
 
-const insertionSort = () => {};
+const insertionSort = (unSortedArray) => {
+  for (let i = 1; i < unSortedArray.length; i++) {
+    const currentNum = unSortedArray[i];
+    let j = i - 1;
+
+    while (j >= 0 && unSortedArray[j] > currentNum) {
+      unSortedArray[j + 1] = unSortedArray[j];
+      j--;
+    }
+    unSortedArray[j + 1] = currentNum;
+  }
+
+  const sortedArray = unSortedArray;
+
+  return sortedArray;
+};
 
 module.exports = { insertionSort };
