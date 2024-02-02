d6187133f4bf2a593b457e64d4aba85d064fdc59
Charuka <itscharukadeshan@gmail.com>
2024-02-02 15:08:19 +0530

    file📦: Add test.js

---
+++ 13_SortingAlgorythms/03_selectionSort/test.js
@@ -0,0 +1,9 @@
+/** @format */
+
+const { selectionSort } = require("./selectionSort");
+
+const unSortedArray = [4, 3, 2, 10, 12, 1, 5, 6];
+
+const result = selectionSort(unSortedArray);
+
+console.log(result);
