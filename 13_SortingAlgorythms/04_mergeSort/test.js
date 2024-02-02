0079957592c82434aa10db04f56e24084a1879b6
Charuka <itscharukadeshan@gmail.com>
2024-02-02 15:22:04 +0530

    file📦: Add test.js

---
+++ 13_SortingAlgorythms/04_mergeSort/test.js
@@ -0,0 +1,9 @@
+/** @format */
+
+const { mergeSort } = require("./mergeSort");
+
+const unSortedArray = [4, 3, 2, 10, 12, 1, 5, 6];
+
+const result = mergeSort(unSortedArray);
+
+console.log(result);
