825ea3bf8235250aaa4566db8430a5584754a39a
Charuka <itscharukadeshan@gmail.com>
2024-02-02 15:03:12 +0530

    docs📝: Add insertionSort examples

--- 13_SortingAlgorythms/02_insertionSort/test.js
+++ 13_SortingAlgorythms/02_insertionSort/test.js
@@ -1,3 +1,9 @@
 /** @format */
 
 const { insertionSort } = require("./insertionSort");
+
+const unSortedArray = [4, 3, 2, 10, 12, 1, 5, 6];
+
+const result = insertionSort(unSortedArray);
+
+console.log(result);
