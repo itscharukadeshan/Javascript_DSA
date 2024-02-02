62d136de585b286905619a723194915cfc45b8d9
Charuka <itscharukadeshan@gmail.com>
2024-02-02 15:36:32 +0530

    file📦: Add mergeSort ( )  function

--- 13_SortingAlgorythms/04_mergeSort/mergeSort.js
+++ 13_SortingAlgorythms/04_mergeSort/mergeSort.js
@@ -1,5 +1,35 @@
 /** @format */
 
-const mergeSort = (array) => {};
+const mergeSort = (array) => {
+  if (array.length <= 1) {
+    return array;
+  }
+  const mid = Math.floor(array.length / 2);
+
+  const left = mergeSort(array.slice(0, mid));
+  const right = mergeSort(array.slice(mid));
+
+  return merge(left, right);
+};
+
+const merge = (left, right) => {
+  const merged = [];
+
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      merged.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      merged.push(right[rightIndex]);
+
+      rightIndex++;
+    }
+  }
+
+  return merged.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+};
 
 module.exports = { mergeSort };
